758b3057a6aeabd375c4bdac2f24449be8f38b9f
Breno Oliveira <breno.oliver07@gmail.com>
2019-08-07 14:55:24 -0300

    estilizando página main

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,5 +1,93 @@
 import React from 'react';
 
-const Main = ({ match }) => <h1>{match.params.id}</h1>;
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+
+const Main = ({ match }) => (
+  <div className="main-container">
+    <img src={logo} alt="Tindev" />
+    <ul>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+          alt=""
+        />
+        <footer>
+          <strong>Filipe Deschamps</strong>
+          <p>Programador e cantor sertanejo nos tempos livres.</p>
+        </footer>
+
+        <div className="buttons">
+          <button type="button">
+            <img src={dislike} alt="Dislike" />
+          </button>
+          <button type="button">
+            <img src={like} alt="Like" />
+          </button>
+        </div>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+          alt=""
+        />
+        <footer>
+          <strong>Filipe Deschamps</strong>
+          <p>Programador e cantor sertanejo nos tempos livres.</p>
+        </footer>
+
+        <div className="buttons">
+          <button type="button">
+            <img src={dislike} alt="Dislike" />
+          </button>
+          <button type="button">
+            <img src={like} alt="Like" />
+          </button>
+        </div>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+          alt=""
+        />
+        <footer>
+          <strong>Filipe Deschamps</strong>
+          <p>Programador e cantor sertanejo nos tempos livres.</p>
+        </footer>
+
+        <div className="buttons">
+          <button type="button">
+            <img src={dislike} alt="Dislike" />
+          </button>
+          <button type="button">
+            <img src={like} alt="Like" />
+          </button>
+        </div>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+          alt=""
+        />
+        <footer>
+          <strong>Filipe Deschamps</strong>
+          <p>Programador e cantor sertanejo nos tempos livres.</p>
+        </footer>
+
+        <div className="buttons">
+          <button type="button">
+            <img src={dislike} alt="Dislike" />
+          </button>
+          <button type="button">
+            <img src={like} alt="Like" />
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Main;
