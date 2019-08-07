5bf5c99a624b01b8c6844a0eaf48cdcfc5592f95
Breno Oliveira <breno.oliver07@gmail.com>
2019-08-07 14:12:07 -0300

    Estilo página de login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './Login.css';
 
 import logo from '../../assets/logo.svg';
 
-const Login = () => <img src={logo} alt="Tindev" />;
+const Login = () => (
+  <div className="login-container">
+    <form>
+      <img src={logo} alt="Tindev" />
+      <input placeholder="Digite seu usuário do Github" />
+      <button type="submit">Enviar</button>
+    </form>
+  </div>
+);
 
 export default Login;
