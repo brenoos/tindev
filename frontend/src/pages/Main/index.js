fc707a2cf59cfb0e22e36b8b5d29f9df8b9fd0ca
Breno Oliveira <breno.oliver07@gmail.com>
2019-08-07 15:27:00 -0300

    likes e dislikes

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -23,29 +23,50 @@ const Main = ({ match }) => {
     loadUser();
   }, [match.params.id]);
 
+  const handleLike = async id => {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: match.params.id },
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  };
+
+  const handleDislike = async id => {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id },
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  };
+
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>
-            <img src={user.avatar} alt={user.name} />
-            <footer>
-              <strong>{user.name}</strong>
-              <p>{user.bio}</p>
-            </footer>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
 
-            <div className="buttons">
-              <button type="button">
-                <img src={dislike} alt="Dislike" />
-              </button>
-              <button type="button">
-                <img src={like} alt="Like" />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        // eslint-disable-next-line jsx-a11y/accessible-emoji
+        <div className="empty">Acabou 😕</div>
+      )}
     </div>
   );
 };
