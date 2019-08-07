b29dfc0228287a5afa4f60a41cc3608658f6d1e4
Breno Oliveira <breno.oliver07@gmail.com>
2019-08-07 13:50:21 -0300

    Assets e componente Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/Login';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <Login />;
 }
 
 export default App;
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg';
+
+const Login = () => <img src={logo} alt="Tindev" />;
+
+export default Login;
