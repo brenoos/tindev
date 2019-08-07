731d175872633a375b02dc368a6f16931fd5e46d
Breno Oliveira <breno.oliver07@gmail.com>
2019-08-07 15:50:17 -0300

    ajustando user para lower case no backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -19,7 +19,7 @@ class DevController {
   }
 
   async store(req, res) {
-    const { username } = req.body;
+    const username = req.body.username.toLowerCase();
 
     const userExists = await Dev.findOne({ user: username });
 
@@ -31,7 +31,12 @@ class DevController {
 
     const { name, bio, avatar_url: avatar } = resp.data;
 
-    const dev = await Dev.create({ name, user: username, bio, avatar });
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar,
+    });
 
     return res.json(dev);
   }
